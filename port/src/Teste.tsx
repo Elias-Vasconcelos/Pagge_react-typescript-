7d9d28ced00cb5028a6e76ead62c693079964b35
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-05-28 18:56:43 -0300

    Instalação do pacortes do projeto

---
+++ port/src/Teste.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste() {
+  return (
+    <>
+      <div>eae pai</div>
+      <Botao>opa</Botao>
+    </>
+  )
+}
+
+export default Teste
